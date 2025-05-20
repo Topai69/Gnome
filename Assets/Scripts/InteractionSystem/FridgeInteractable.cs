d2401393cc6f23983cdf473cc66ba725976a92f7
TudorT699 <tudor0092@gmail.com>
2025-05-20 03:48:29 +0200

    TaskBar (now works in the new scene as well)

--- Assets/Scripts/InteractionSystem/FridgeInteractable.cs
+++ Assets/Scripts/InteractionSystem/FridgeInteractable.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class FridgeInteractable : InteractableBase
 {
+    [SerializeField] private AnimationClip rotation;
+    [SerializeField] private TaskManager taskManager;
+    [HideInInspector] public ScoreScript ScoreScript; 
+
     /*private void Start()
     {
         anim = transform.parent.GetComponent<Animation>();
         anim.AddClip(rotation, "Rotating");
     }*/
+
+    private void Start()
+    {
+        ScoreScript = FindAnyObjectByType<ScoreScript>();
+    }
 
     public override void OnInteract()
     {
@@ -14,5 +23,15 @@ public class FridgeInteractable : InteractableBase
         Debug.Log("Interacted with fridge");
         //anim.Play("Rotating");
         ((transform.parent).parent.gameObject.GetComponent<Animation>()).Play("Fridge");
+
+        if (taskManager != null)
+        {
+            taskManager.CompleteTask(0); 
+        }
+
+        if (ScoreScript != null)
+        {
+            ScoreScript.Score += 20;
+        }
     }
 }
