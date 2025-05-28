7411d3c10ed6a4a07a1565a89c59ef647852808d
paglietta <anthonyelia@outlook.it>
2025-05-28 18:05:00 +0200

    ft: forgot saving

--- Assets/Scripts/Animation Test.cs
+++ Assets/Scripts/Animation Test.cs
@@ -3,25 +3,27 @@ using UnityEngine;
 public class AnimationTest : MonoBehaviour
 {
     private Animator Gnome;
-    private float movementThreshold = 0.1f; 
-    
+    private float movementThreshold = 0.1f;
+
     void Start()
     {
         Gnome = GetComponent<Animator>();
     }
-    
+
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
- 
-        bool isMoving = Mathf.Abs(horizontalInput) > movementThreshold || 
+
+        bool isMoving = Mathf.Abs(horizontalInput) > movementThreshold ||
                         Mathf.Abs(verticalInput) > movementThreshold;
-        
+
         Gnome.SetBool("IsIdle", !isMoving);
         Gnome.SetBool("IsWalking", isMoving);
-        
+
         float movementSpeed = new Vector2(horizontalInput, verticalInput).magnitude;
         Gnome.SetFloat("MovementSpeed", movementSpeed);
     }
 }
+
+
