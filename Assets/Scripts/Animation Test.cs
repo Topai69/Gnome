70ae97f959050b7d3c01835efeaeaa2c85043bbd
Rice <julapiczulska@gmail.com>
2025-05-28 18:00:49 +0200

    ft: idle & walking animations

--- Assets/Scripts/Animation Test.cs
+++ Assets/Scripts/Animation Test.cs
@@ -1,29 +1,27 @@
 using UnityEngine;
-using UnityEngine.Windows;
 
 public class AnimationTest : MonoBehaviour
 {
     private Animator Gnome;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float movementThreshold = 0.1f; 
+    
     void Start()
     {
         Gnome = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (UnityEngine.Input.GetKeyDown(KeyCode.T))
-        {
-            Gnome.SetBool("IsIdle",true);
-            Debug.Log("T Pressed");
-        }
-
-
-    //    if (animator.SetTrigger((Input.GetKeyDown("t")));
-    //    {
-    //        animator.Play("Gnome");
-    //    }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+ 
+        bool isMoving = Mathf.Abs(horizontalInput) > movementThreshold || 
+                        Mathf.Abs(verticalInput) > movementThreshold;
+        
+        Gnome.SetBool("IsIdle", !isMoving);
+        Gnome.SetBool("IsWalking", isMoving);
+        
+        float movementSpeed = new Vector2(horizontalInput, verticalInput).magnitude;
+        Gnome.SetFloat("MovementSpeed", movementSpeed);
     }
 }
