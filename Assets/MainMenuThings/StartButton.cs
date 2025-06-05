757ca105ae729678e948e78aaf768059afebb971
TudorT699 <tudor0092@gmail.com>
2025-06-05 19:11:34 +0200

    Added Loading Screen -> Main Menu Scene

--- Assets/MainMenuThings/StartButton.cs
+++ Assets/MainMenuThings/StartButton.cs
@@ -1,23 +1,14 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class StartButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject Video;
     public GameObject Image;
     public GameObject VideoPlayer;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject LoadingScreen; 
+    public float delayBeforeLoad = 5f; // Delay Lenght
 
     public void StartGame()
     {
@@ -25,17 +16,23 @@ public class StartButton : MonoBehaviour
         Image.SetActive(false);
         Video.SetActive(true);
         VideoPlayer.SetActive(true);
+        if (LoadingScreen != null) LoadingScreen.SetActive(true);
     }
+
     IEnumerator ChangeScene()
-    { 
-        //yield return new WaitForSeconds(3f); // 3 seconds delay
-        SceneManager.LoadScene(2);
-        VideoPlayer.SetActive(false);
-        yield return null;
-    }   
+    {
+        yield return new WaitForSeconds(delayBeforeLoad);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(2);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
     public void ExitGame()
     {
         Application.Quit();
     }
-    
 }
