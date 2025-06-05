5c3e314690df6352fc87f71ff3cd51236ce096b3
paglietta <anthonyelia@outlook.it>
2025-06-06 00:53:51 +0200

    ft: fixed Y button bug

--- Assets/Scripts/Animation Test.cs
+++ Assets/Scripts/Animation Test.cs
@@ -35,12 +35,19 @@ public class AnimationTest : MonoBehaviour
 
     void Update()
     {
+        //i blocked the Y button on controller cause it was ccausing some weird shi
+        if (Input.GetKeyDown(KeyCode.JoystickButton3) || Input.GetKey(KeyCode.JoystickButton3))
+        {
+            return;
+        }
+
         if (!playerMovement.blockAInput)
         { 
             if (isPushing)
                 return;
                 
-            if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.JoystickButton1)) && playerMovement != null && playerMovement.grounded)
+            if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.JoystickButton1)) && 
+                playerMovement != null && playerMovement.grounded)
             {
                 jumpInitiated = true;
                 jumpInitiationTimer = 0.2f;
@@ -50,61 +57,61 @@ public class AnimationTest : MonoBehaviour
                 Gnome.SetBool("IsFalling", false);
             }
 
-        if (jumpInitiationTimer > 0)
-        {
-            jumpInitiationTimer -= Time.deltaTime;
-        }
-
-        if (playerMovement != null && !playerMovement.grounded)
-        {
-            if (playerMovement.GetVerticalVelocity() > 0 || jumpInitiated)
+            if (jumpInitiationTimer > 0)
             {
-                Gnome.SetBool("IsJumping", true);
-                Gnome.SetBool("IsFalling", false);
+                jumpInitiationTimer -= Time.deltaTime;
+            }
 
-                if (playerMovement.GetVerticalVelocity() > 0)
+            if (playerMovement != null && !playerMovement.grounded)
+            {
+                if (playerMovement.GetVerticalVelocity() > 0 || jumpInitiated)
                 {
+                    Gnome.SetBool("IsJumping", true);
+                    Gnome.SetBool("IsFalling", false);
+
+                    if (playerMovement.GetVerticalVelocity() > 0)
+                    {
+                        jumpInitiated = false;
+                    }
+                }
+                else
+                {
+                    Gnome.SetBool("IsJumping", false);
+                    Gnome.SetBool("IsIdle", false);
+                    Gnome.SetBool("IsWalking", false);
+                    Gnome.SetBool("IsFalling", true);
                     jumpInitiated = false;
                 }
+                Gnome.SetFloat("MovementSpeed", 0f);
+                return;
             }
             else
             {
-                Gnome.SetBool("IsJumping", false);
-                Gnome.SetBool("IsIdle", false);
-                Gnome.SetBool("IsWalking", false);
-                Gnome.SetBool("IsFalling", true);
-                jumpInitiated = false;
+                if (jumpInitiationTimer <= 0)
+                {
+                    jumpInitiated = false;
+                }
+
+                if (!jumpInitiated)
+                {
+                    Gnome.SetBool("IsJumping", false);
+                    Gnome.SetBool("IsFalling", false);
+                }
             }
-            Gnome.SetFloat("MovementSpeed", 0f);
-            return;
-        }
-        else
-        {
-            if (jumpInitiationTimer <= 0)
-            {
-                jumpInitiated = false;
-            }
+
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            bool isMoving = Mathf.Abs(horizontalInput) > movementThreshold || Mathf.Abs(verticalInput) > movementThreshold;
 
             if (!jumpInitiated)
             {
-                Gnome.SetBool("IsJumping", false);
-                Gnome.SetBool("IsFalling", false);
+                Gnome.SetBool("IsIdle", !isMoving);
+                Gnome.SetBool("IsWalking", isMoving);
             }
-        }
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        bool isMoving = Mathf.Abs(horizontalInput) > movementThreshold || Mathf.Abs(verticalInput) > movementThreshold;
-
-        if (!jumpInitiated)
-        {
-            Gnome.SetBool("IsIdle", !isMoving);
-            Gnome.SetBool("IsWalking", isMoving);
-        }
-
-        float movementSpeed = new Vector2(horizontalInput, verticalInput).magnitude;
-        Gnome.SetFloat("MovementSpeed", movementSpeed);
+            float movementSpeed = new Vector2(horizontalInput, verticalInput).magnitude;
+            Gnome.SetFloat("MovementSpeed", movementSpeed);
         }
     }
 }
