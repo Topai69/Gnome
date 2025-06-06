74f0df8fc49723848bc23c3a3c35c3f3a74e0f81
Topai69 <ivanxtopai@gmail.com>
2025-06-06 03:55:54 +0200

    new menu

--- Assets/UI Folder/PauseMenu/AudioManager.cs
+++ Assets/UI Folder/PauseMenu/AudioManager.cs
@@ -4,6 +4,8 @@ public class AudioManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Slider audioSlider;
+    public GameObject gameplayUI;
+    public bool settingsUI;
     void Start()
     {
         if (!PlayerPrefs.HasKey("musicVolume"))
@@ -16,7 +18,19 @@ public class AudioManager : MonoBehaviour
             Load();
         }
     }
-
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && gameplayUI.activeInHierarchy)
+        {
+            gameplayUI.SetActive(false);
+            settingsUI = true;
+        }
+        if (settingsUI == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameplayUI.SetActive(true);
+            settingsUI = false;
+        }
+    }
     public void ChangeVolume()
     {
         AudioListener.volume = audioSlider.value;
