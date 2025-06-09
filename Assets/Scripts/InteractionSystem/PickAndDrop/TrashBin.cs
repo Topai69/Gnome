42a1362e93de83d6cfdddd3fa10d500e3d17ac02
TudorT699 <tudor0092@gmail.com>
2025-06-09 15:44:06 +0200

    Fixed Trash Objs Interaction

--- Assets/Scripts/InteractionSystem/PickAndDrop/TrashBin.cs
+++ Assets/Scripts/InteractionSystem/PickAndDrop/TrashBin.cs
@@ -3,14 +3,10 @@ using UnityEngine;
 public class TrashBin : MonoBehaviour
 {
     [SerializeField] private TrashType acceptedType;
-    [SerializeField] private PopUpManager popUpManager;
     
     private void Start()
     {
-        if (popUpManager == null)
-        {
-            popUpManager = FindObjectOfType<PopUpManager>();
-        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,9 +35,6 @@ public class TrashBin : MonoBehaviour
         screenPos.z = -Camera.main.transform.position.z;
         
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-        
-        GameObject popUpObject = Instantiate(popUpManager.GetPopUpPrefab(), worldPos, Quaternion.identity);
-        popUpObject.GetComponent<TrashBinPopup>().text_value = "WRONG BIN";
         
         if (item.TryGetComponent(out Rigidbody rb))
         {
